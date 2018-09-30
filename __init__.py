8097ff31a33b895ffe0d5e1a6ee1f627aca82e43
Daniel Kratzert <dkratzert@gmx.de>
2018-09-30 15:15:40 +0200

    __init__.py fixes

--- __init__.py
+++ __init__.py
@@ -1,19 +1,21 @@
 
-from shelxfile.shelx import __version__
-from shelxfile.shelx import __doc__
-from shelxfile.shelx import *
-from shelxfile.atoms import *
-from shelxfile.misc import *
-from shelxfile.cards import *
-from shelxfile.dsrmath import *
-from refine.shx_refine import *
+from .shelxfile.shelx import __version__
+from .shelxfile.shelx import __doc__
+from .shelxfile.shelx import *
+from .shelxfile.atoms import *
+from .shelxfile.misc import *
+from .shelxfile.cards import *
+from .shelxfile.dsrmath import *
+from .refine.shx_refine import *
 
 __all__ = [
     "ShelXFile",
     "Atoms",
     "Atom",
     "frac_to_cart",
-    "cart_to_frac"]
+    "cart_to_frac",
+    "SDM",
+    "elements"]
 
 if __name__ == "__main__":
     ver = sys.version_info
