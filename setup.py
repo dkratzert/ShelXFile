1b8e87588d884cb41115136751d651428d4c537c
Daniel Kratzert <dkratzert@gmx.de>
2021-05-23 17:41:24 +0200

    prepare setup.py for release

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ShelXFile',
     version='1',
-    packages=['fit', 'draw', 'refine', 'shelxfile'],
-    url='',
+    url='https://github.com/dkratzert/ShelXFile',
     license='Beerware License',
     author='Daniel Kratzert',
     author_email='dkratzert@gmx.de',
-    description='A parser for SHELXL results files'
+    description='A parser for SHELXL results files.',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.5",
 )
