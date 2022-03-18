06226755e3bfcb8fcea01c53df35599507bb3bcb
Daniel Kratzert <dkratzert@gmx.de>
2022-03-18 10:59:03 +0100

    fix path

--- shelxfile/tests/test_parse.py
+++ shelxfile/tests/test_parse.py
@@ -6,7 +6,7 @@ from shelxfile import Shelxfile
 class ParseTestCase(unittest.TestCase):
     def test_parse_hklf_after_uncloded_resi(self):
         shx = Shelxfile()
-        shx.read_file(r'shelxfile/tests/resources/unclosed_resi.res')
+        shx.read_file(r'tests/resources/unclosed_resi.res')
         self.assertEqual('HKLF 4 1  1 0 0 0 1 0 0 0 1  1 0', shx.hklf.__repr__())  # add assertion here
 
 
