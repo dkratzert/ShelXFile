6c4d539cbe37b24bf5b027cd75cdcd2662562be4
Daniel Kratzert <dkratzert@gmx.de>
2022-07-08 16:36:05 +0200

    Update upload_to_pypi.bat

--- scripts/upload_to_pypi.bat
+++ scripts/upload_to_pypi.bat
@@ -1,2 +1,5 @@
-cd ..
-python3 -m twine upload --repository pypi dist\*
+rem cd ..
+call venv\Scripts\activate.bat
+venv\Scripts\pip install twine
+rem cd ..
+venv\Scripts\python -m twine upload --repository pypi dist\*
