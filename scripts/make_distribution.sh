f6fa368a01e13cede4fdc1a96e223c5d903482d1
Daniel Kratzert <dkratzert@gmx.de>
2022-10-02 17:52:05 +0200

    add build to dist script

--- scripts/make_distribution.sh
+++ scripts/make_distribution.sh
@@ -4,5 +4,6 @@ rm -Rf dist
 rm -Rf shelxfile.egg-info
 pip install pip -U
 pip install --upgrade setuptools
+python3 -m pip install --upgrade build
 python3 -m build
 # python3 -m twine upload --repository pypi dist/*
