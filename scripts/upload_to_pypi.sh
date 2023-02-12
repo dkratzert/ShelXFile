2f423bc292dbc7170ca2db971eb0b795b52c8414
Daniel Kratzert <dkratzert@gmx.de>
2023-02-12 12:33:34 +0100

    Improves test-release scripts

--- scripts/upload_to_pypi.sh
+++ scripts/upload_to_pypi.sh
@@ -1,5 +1,6 @@
 ####
 source venv/bin/activate
 python3 -m twine upload --repository shelxfile dist/*
-git tag v"$(cat ./setup.cfg | grep version | cut -d' ' -f3)"
-echo "Now push the tag!"
+VERSION="$(cat ./setup.cfg | grep version | cut -d' ' -f3)"
+git tag v$VERSION
+echo "Tag pushed, a release should appear on https://test.pypi.org/project/shelxfile/$VERSION"
