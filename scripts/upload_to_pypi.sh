39a5f068ced3b59fe07a468c4113d01a70652731
Daniel Kratzert <dkratzert@gmx.de>
2023-02-12 20:42:51 +0100

    Update upload_to_pypi.sh

--- scripts/upload_to_pypi.sh
+++ scripts/upload_to_pypi.sh
@@ -1,6 +1,6 @@
 ####
 source venv/bin/activate
 python3 -m twine upload --repository shelxfile dist/*
-VERSION="$(cat ./setup.cfg | grep version | cut -d' ' -f3)"
-git tag v$VERSION
-echo "Tag pushed, a release should appear on https://test.pypi.org/project/shelxfile/$VERSION"
+#VERSION="$(cat ./setup.cfg | grep version | cut -d' ' -f3)"
+#git tag v$VERSION
+echo "Tag pushed, a release should appear on https://test.pypi.org/project/shelxfile"
